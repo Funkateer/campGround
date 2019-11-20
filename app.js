641ee82b3468be1a6f84e2fb73b910906d6ed295
Alà Eddin Abugrara <alaeddin.abugrara@gmail.com>
2019-11-20 22:39:22 +0100

    add .gitignore replace readme

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.use("/", authRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
+//listen to port
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("The YelpCamp v8.0 Server Has Started aaand deployed .. hopefully!");
 });
